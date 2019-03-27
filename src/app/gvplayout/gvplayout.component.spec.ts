d894e254d578b7701f2eccf3de541d9d09747296
Ivan Razumov <ivan.razumov@cern.ch>
2019-03-27 13:15:18 +0100

    Missed one reference to 'template'

--- src/app/gvplayout/gvplayout.component.spec.ts
+++ src/app/gvplayout/gvplayout.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GvptemplateComponent } from './gvptemplate.component';
+import { GvplayoutComponent } from './gvplayout.component';
 
-describe('GvptemplateComponent', () => {
-  let component: GvptemplateComponent;
-  let fixture: ComponentFixture<GvptemplateComponent>;
+describe('GvplayoutComponent', () => {
+  let component: GvplayoutComponent;
+  let fixture: ComponentFixture<GvplayoutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GvptemplateComponent ]
+      declarations: [ GvplayoutComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GvptemplateComponent);
+    fixture = TestBed.createComponent(GvplayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
