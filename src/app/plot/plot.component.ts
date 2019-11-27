ea6cdc58857cb6629ee735fb2d6a5888e48b25c3
Grigory <thegriglat@gmail.com>
2019-11-27 12:33:33 +0300

    add floating plot hint

--- src/app/plot/plot.component.ts
+++ src/app/plot/plot.component.ts
@@ -1,7 +1,19 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { GVPAPIService } from '../services/gvpapi.service';
-import { GvpPngRequest } from '../classes/gvp-plot';
+import { GvpPngRequest, GvpJSON, GvpPlot, GvpParameter } from '../classes/gvp-plot';
 import { Observable } from 'rxjs';
+
+interface HintData {
+  observable: string,
+  beam: string,
+  target: string,
+  beam_energy: string,
+  items: {
+    version: string,
+    model: string,
+    parameters: GvpParameter[],
+  }[]
+}
 
 /**
  * Container for a single plot. WIP.
@@ -18,6 +30,9 @@ export class PlotComponent implements OnInit {
   url: string = "";
   status = false;
   inProgress = false;
+  showHint = false;
+  private hintData: HintData = null;
+  private mousePos: { x: number, y: number } = { x: 0, y: 0 };
 
   constructor(private api: GVPAPIService) {
   }
@@ -25,7 +40,11 @@ export class PlotComponent implements OnInit {
   ngOnInit() {
     if (this.configObs) {
       this.configObs.subscribe(e => {
-        if (e) this.doStuff(e);
+
+        if (e && e.data.length !== 0) {
+          this.doStuff(e);
+          this.hintData = this.setHintData(e.data);
+        }
       });
 
     }
@@ -43,6 +62,45 @@ export class PlotComponent implements OnInit {
       console.log(`url = ${this.url}`);
     })
   }
+
+  hint(event: MouseEvent) {
+    this.showHint = true;
+    this.mousePos.x = event.offsetX + 100;
+    this.mousePos.y = event.offsetY + 15;
+  }
+
+  hideHint() {
+    this.showHint = false;
+  }
+
+  getMousePos(): { x: string, y: string } {
+    return {
+      x: `${this.mousePos.x}px`,
+      y: `${this.mousePos.y}px`
+    }
+  }
+
+  private setHintData(p: GvpJSON[]): HintData {
+    const r: HintData = {
+      observable: p[0].metadata.observableName,
+      beam: p[0].metadata.beamParticle,
+      target: p[0].metadata.targetName,
+      beam_energy: p[0].metadata.beam_energy_str,
+      items: p.map(e => {
+        return {
+          version: e.mctool.version,
+          model: e.mctool.model,
+          parameters: e.metadata.parameters
+        }
+      })
+    }
+    return r;
+  }
+
+  getHintData(): HintData {
+    return this.hintData;
+  }
+
   /*
     draw_old() {
       this.service.getPlot_old(this.config, this.testId, this.versionId, this.useMarkers).subscribe(
