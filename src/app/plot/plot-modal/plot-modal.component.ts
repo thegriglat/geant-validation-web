89fda336678997a8ccda13748021e27b41024384
Grigory <thegriglat@gmail.com>
2019-12-03 11:57:14 +0300

    Show expname in modal

--- src/app/plot/plot-modal/plot-modal.component.ts
+++ src/app/plot/plot-modal/plot-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { GvpPngRequest, GvpJSON } from 'src/app/classes/gvp-plot';
 import { GVPAPIService } from 'src/app/services/gvpapi.service';
+import { from, forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 declare var JSROOT: any;
 
@@ -19,14 +21,32 @@ export class PlotModalComponent implements OnInit {
   selectedRef: GvpJSON = null;
   inProgress = false;
 
+  private _names = new Map<GvpJSON, string>();
+
   constructor(private api: GVPAPIService) { }
 
   ngOnInit() {
+    // fill _names
+    forkJoin(
+      this.config.data.map(j => this._getName(j).pipe(
+        map(e => {
+          return { json: j, name: e };
+        })
+      ))
+    ).subscribe(list => {
+      for (const e of list)
+        this._names.set(e.json, e.name);
+    })
   }
 
   emit() {
     this.out.emit(false);
   }
+
+  getName(p: GvpJSON) {
+    return (this._names.has(p)) ? this._names.get(p) : "exp. data";
+  }
+
   switchModalRoot() {
     let h = 640, w = 480;
     // keep png image size before draw jsroot
@@ -63,7 +83,11 @@ export class PlotModalComponent implements OnInit {
     })
   }
 
-  getName(p: GvpJSON): string {
-    return `${p.mctool.version} ${p.mctool.model}`;
+  private _getName(p: GvpJSON) {
+    if (p.mctool.model !== "experiment")
+      return from([`${p.mctool.version} ${p.mctool.model}`]);
+    return this.api.inspireById(p.article.inspireId).pipe(
+      map(e => (e.expname) ? e.expname : "exp. data")
+    )
   }
 }
