e0fb1943102627b56b44044a163a68aeac9b4a4d
Grigory <thegriglat@gmail.com>
2020-03-17 14:05:02 +0300

    Add version menu

--- src/app/stat-comparison/stat-comparison.component.ts
+++ src/app/stat-comparison/stat-comparison.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap, map } from 'rxjs/operators';
-import { Nullable, GvpTest } from '../classes/gvp-plot';
+import { Nullable, GvpTest, GvpMctoolNameVersion } from '../classes/gvp-plot';
 import { GVPAPIService } from '../services/gvpapi.service';
 import { Observable } from 'rxjs';
+import { unstableVersionFilter, versionSorter } from '../utils';
 
 @Component({
   selector: 'app-stat-comparison',
@@ -13,14 +14,95 @@ import { Observable } from 'rxjs';
 export class StatComparisonComponent implements OnInit {
 
   public test: Observable<GvpTest> = new Observable<GvpTest>();
+  public menuVersions: GvpMctoolNameVersion[] = [];
+  public versionsSel: GvpMctoolNameVersion[] = [];
+  public showUnstableVersions = false;
+
+  private MCToolNameCache = new Map<number, string>();
+  /** Cache of MC tool versions, popuated on page load
+   * key: database ID (used in API requests)
+   * release_date field not used
+   */
+  private MCToolNameVersionCache = new Map<number, { version: string, mctool_name_id: number, release_date: string }>();
 
   constructor(private route: ActivatedRoute, private router: Router, private api: GVPAPIService) { }
 
   ngOnInit() {
+    // Populate caches
+    this.api.mctool_name_version().subscribe(response => {
+      for (const elem of response) {
+        this.MCToolNameVersionCache.set(elem.mctool_name_version_id, {
+          version: elem.version,
+          mctool_name_id: elem.mctool_name_id,
+          release_date: elem.release_date
+        });
+      }
+    });
+
+    this.api.mctool_name().subscribe(response => {
+      for (const elem of response) {
+        this.MCToolNameCache.set(elem.mctool_name_id, elem.mctool_name_name);
+      }
+    });
     this.test = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) => 
+      switchMap((params: ParamMap) =>
         this.api.testById(Number(params.get('id')))
       ));
+    this.test.subscribe(test => {
+      this.updateVersionMenu(test);
+    })
+  }
+
+  unstableFilter(v: GvpMctoolNameVersion[]): GvpMctoolNameVersion[] {
+    if (this.showUnstableVersions)
+      return v;
+    return v.filter(unstableVersionFilter);
+  }
+
+
+  filterVersionSel() {
+    if (!this.showUnstableVersions) return;
+    this.versionsSel = this.versionsSel.filter(unstableVersionFilter)
+  }
+
+  versionSelectFilter(items: GvpMctoolNameVersion[], query: string): GvpMctoolNameVersion[] {
+    return items.filter(e => e.version.indexOf(query) !== -1);
+  }
+
+  versionFormatter(item: GvpMctoolNameVersion, query?: string): string {
+    return item.version;
+  }
+
+  updateVersionMenu(test: GvpTest): void {
+    this.api.uniqlookup_version(test.test_id).subscribe((response) => {
+      this.menuVersions = [];
+      const versions = new Map<number, string>();
+      for (const i of response) {
+        if (this.menuVersions.map(e => e.mctool_name_version_id).indexOf(i) === -1) {
+          const result = this.MCToolNameVersionCache.get(i);
+          if (result === undefined) {
+            console.log(`mctool.version ${i} not found!`);
+            continue;
+          }
+          const version = result.version;
+          const mctoolNameId = result.mctool_name_id;
+          const name = this.MCToolNameCache.get(mctoolNameId);
+          versions.set(i, `${name}: ${version}`);
+          this.menuVersions.push(
+            {
+              mctool_name_version_id: i,
+              version: result.version,
+              mctool_name_id: result.mctool_name_id,
+              release_date: result.release_date
+            }
+          );
+        }
+      }
+      this.menuVersions.sort(versionSorter).reverse();
+      if (this.menuVersions.length === 1) {
+        this.versionsSel = this.menuVersions.slice();
+      }
+    });
   }
 
 }
