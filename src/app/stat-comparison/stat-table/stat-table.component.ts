df243e62e18bcfced0d133df1658aa0e159f1dc8
Grigory <thegriglat@gmail.com>
2020-03-18 21:14:54 +0300

    first modal plots in stat

--- src/app/stat-comparison/stat-table/stat-table.component.ts
+++ src/app/stat-comparison/stat-table/stat-table.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { GVPAPIService } from 'src/app/services/gvpapi.service';
-import { GvpTest, GvpMctoolNameVersion, ParametersList, GvpPlotIdRequest, Nullable, GvpJSON } from 'src/app/classes/gvp-plot';
+import { GvpTest, GvpMctoolNameVersion, ParametersList, GvpPlotIdRequest, Nullable, GvpJSON, GvpPngRequest } from 'src/app/classes/gvp-plot';
 import { forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { cartesian } from 'src/app/utils';
 import { getEstimator, Estimator } from './../estimator';
+import { SuiModalService } from 'ng2-semantic-ui';
+import { PlotModal } from 'src/app/plot/plot-modal/plot-modal.component';
 
 @Component({
   selector: 'app-stat-table',
@@ -22,7 +24,7 @@ export class StatTableComponent implements OnInit {
   public estimator: Nullable<Estimator> = getEstimator('chi2');
   public combinations: string[][] = [];
 
-  constructor(private api: GVPAPIService) { }
+  constructor(private api: GVPAPIService, private modalService: SuiModalService) { }
 
   ngOnInit() {
     if (!this.test) return;
@@ -63,24 +65,48 @@ export class StatTableComponent implements OnInit {
       // todo;
     }
   */
-  ParamC2ParameterList(combination: string[][]): ParametersList {
+  ParamC2ParameterList(combination: string[]): ParametersList {
     if (!this.parameters) return [];
     // TODO: dirty typing hack: string[] vs string
-    let res: any[] = [];
+    let res: ParametersList = [];
     const startN = 5; // target model sec beam obs
     const values = combination.slice(startN);
     const keys = this.parameters.map(e => e[0]);
     for (let idx = 0; idx < keys.length; idx++)
       res.push([keys[idx], [values[idx]]]);
-    return res as ParametersList;
+    return res;
   }
 
-  ParamC2str(combination: string[][]): string {
+  ParamC2str(combination: string[]): string {
     let s: string[] = [];
     for (let elem of this.ParamC2ParameterList(combination)) {
       s.push(elem[0] + "=" + elem[1][0]);
     }
     return s.join(", ");
+  }
+
+  showModalPlot(combination: string[]): void {
+    if (!this.test) return;
+    const query: GvpPlotIdRequest = new GvpPlotIdRequest(
+      [this.test.test_id], // testid
+      combination[0], // target
+      this.versions.map(v => v.mctool_name_version_id), // versions
+      [combination[1]], // model
+      [combination[2]], // secs
+      this.beam, // beams
+      [combination[4]], // observ
+      this.ParamC2ParameterList(combination), // parameters
+      [combination[3]] // beamenergy
+    );
+    this.api.getPlotJSON(query).subscribe(jsons => {
+      const config: GvpPngRequest = new GvpPngRequest(jsons);
+      this.api.getPNG(config).subscribe(res => {
+        const url = res.filename;
+        this.modalService.open(
+          new PlotModal(url, config)
+        )
+      })
+    })
   }
 
 }
