440af6bf11daffb7ebfe858a4dafe071d9d7d8e2
Grigory <thegriglat@gmail.com>
2020-05-19 19:39:31 +0300

    Add new estimator

--- src/app/plot/ratiofunctions.ts
+++ src/app/plot/ratiofunctions.ts
@@ -7,13 +7,26 @@ interface RatioFunctions {
     fn: { (base: GvpJSON, ref: GvpJSON): number };
 }
 
-const RatioMaxDiff: RatioFunctions = {
+const RatioSum: RatioFunctions = {
     description: "\\sum \\frac{\\Delta y}{y_{ref}}",
     fn: (base: GvpJSON, ref: GvpJSON) => {
         const [rx1, ry1, ry1err, rx2, ry2, ry2err] = getCommonXY(base, ref);
         let diff = 0;
         for (let i = 0; i < ry2.length; i++) {
             diff += ry2[i] !== 0 ? Math.abs((ry1[i] - ry2[i]) / ry2[i]) : 0;
+        }
+        return diff;
+    }
+}
+
+const RatioMaxDiff: RatioFunctions = {
+    description: "max(\\frac{\\Delta y}{y_{ref}})",
+    fn: (base: GvpJSON, ref: GvpJSON) => {
+        const [rx1, ry1, ry1err, rx2, ry2, ry2err] = getCommonXY(base, ref);
+        let diff = 0;
+        for (let i = 0; i < ry2.length; i++) {
+            const t = ry2[i] !== 0 ? Math.abs((ry1[i] - ry2[i]) / ry2[i]) : 0;
+            if (t > diff) diff = t;
         }
         return diff;
     }
